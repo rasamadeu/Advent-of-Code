1344ba14ee82ce451b1e0541af21acd0e501b72e
rasamadeu <rasamadeu1@hotmail.com>
2024-01-23 02:17:47 +0000

    Committed .c files and input data of the 1st problem of 2015

---
+++ C/2015_1_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+	int floor = 0, counter = 0;
+	char c;
+	FILE *fid;
+
+	fid = fopen("2015_1_input.dat", "rt");
+	while(fscanf(fid, "%c", &c) == 1 && floor > -1){
+		if(c == '('){
+			floor += 1;
+		}
+		if(c == ')'){
+			floor -= 1;
+		}
+		counter++;
+	}
+	fclose(fid);
+	printf("%d\n", counter);
+
+	return 0;
+}
